2de0c44024b255653bf83fc4d031f400b9ef522c
tonarinohito <rsp44316@gmail.com>
2018-08-14 05:37:15 +0900

    選択したユニットの移動範囲を表示

--- SRPG/Source.cpp
+++ SRPG/Source.cpp
@@ -2,11 +2,30 @@
 #include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct INT2
 {
 	int x = 0, y = 0;
 };
+
+//方角
+enum DIRECTION
+{
+	NORTH,
+	WEST,
+	SOUTH,
+	EAST,
+	DIRECTION_MAX
+};
+int directions[][2] =
+{
+	{0, -1},	//NORTH
+	{-1, 0},	//WEST
+	{0,  1},	//SOUTH
+	{1,  0},	//EAST
+};
+
 //地形
 enum MAP
 {
@@ -20,6 +39,7 @@ enum MAP
 	CELL_CASTLE,
 	CELL__MAX
 };
+
 //地形の定義
 struct CELL_DESC
 {
@@ -59,7 +79,10 @@ char mapData[] = "\
 000015500001100000000000220000\
 000000000000000000000000000000";
 
+//セルの最大数
 int cells[MAP_HEIGHT][MAP_WIDTH];
+//trueなら移動範囲を塗りつぶす
+bool fill[MAP_HEIGHT][MAP_WIDTH];
 
 //職業
 enum JOB
@@ -86,15 +109,15 @@ struct JOBDESC
 
 JOBDESC jobDesc[] =
 {
-	//名前		  AA      海　 草　 林　山　 高　 町　 砦　 城
-	{ "ロード",	  "君",  { -1,  1,  2,  4,  -1,   1,  2,  2, }},
-	{ "パラディン","聖",  { -1,  1,  2,  4,  -1,   1,  2,  2, }},
-	{ "Sナイト",   "騎",  { -1,  1,  2,  4,  -1,   1,  2,  2, }},
-	{ "Aナイト",   "重",  { -1,  1,  2,  4,  -1,   1,  2,  2, }},
-	{ "アーチャー","射", { -1,  1,  2,  4,  -1,   1,  2,  2, } },
-	{ "Pナイト",	   "天",  { -1,  1,  2,  4,  -1,   1,  2,  2, } },
-	{ "海賊",	   "賊",  { -1,  1,  2,  4,  -1,   1,  2,  2, } },
-	{ "ハンター",  "狩",   { -1,  1,  2,  4,  -1,   1,  2,  2, } },
+	//名前		  AA      海　 草　 林　 山　 高　  町　 砦　 城
+	{ "ロード",	  "君",  { -1,  1,  2,   4,  -1,   1,  2,  2, }},
+	{ "パラディン","聖",  { -1,  1,  3,   3,  -1,   1,  2,  2, }},
+	{ "Sナイト",   "騎",  { -1,  1,  3,  -1,  -1,   1,  2,  2, }},
+	{ "Aナイト",   "重",  { -1,  1,  2,  -1,  -1,   1,  2,  2, }},
+	{ "アーチャー", "射", {  -1,  1,  3, -1,  -1,   1,  2,  2, } },
+	{ "Pナイト",	   "天",  {  1,  1,  1,  1,   1,   1,  1,  1, } },
+	{ "海賊",	   "賊",  {  2,  1,  1,  2,   4,   1,  1,  2, } },
+	{ "ハンター",  "狩",   { -1,  1,  2,  3,  -1,   1,  2,  2, } },
 	{ "盗賊",	  "盗",   { -1,  1,  2,  4,  -1,   1,  2,  2, }},
 };
 
@@ -140,6 +163,14 @@ WEAPONDESC weaponDescs[] =
 	{ "はがねのおの" , 9,  9,   70,  0,  1,  1 },
 };
 
+//敵か味方か
+enum TEAM
+{
+	PLAYER,
+	ENEMY,
+	TEAM_MAX
+};
+
 //キャラクター
 struct UNIT
 {
@@ -153,34 +184,35 @@ struct UNIT
 	int luck;
 	int defence;
 	int move;
+	TEAM team;
 	WEAPON weapon;
 	INT2 pos;	//初期位置
 };
 constexpr int UNIT_MAX = 21;
 UNIT units[] =
 {
-	//名前		   職業　　　 HP  STR SKI WLV AGI LUC DEF MOV WEP
-	{ "マルス",	   LORD,     18, 5,   3,  5,  7,  7,  7,  7, RAPIRE },
-	{ "ジェイガン", PARADIN,  20, 7,   10, 10, 8,  1,  9, 10, IRON_SWORD },
-	{ "カイン",	   S_KINGHT, 18, 7,   5,  6,  7,  2,  7,  9, IRON_SWORD },
-	{ "アベル",	   S_KINGHT, 18, 6,   7,  6,  7,  2,  7,  7, IRON_SWORD },
-	{ "ドーガ",	   A_KINGHT, 18, 7,   3,  4,  3,  1,  11, 5, IRON_SWORD },
-	{ "ゴードン",   ARCHER,   16, 5,   3,  5,  4,  4,  6,  5, BOW },
-	{ "シーダ",	   P_KINGHT, 16, 3,   6,  7,  12,  9,  7, 8, IRON_SWORD },
-	{ "ガザック",   PIRATE,   24, 7,   3,  7,  8,  0,  6,  6, STEAL_AX },
-	{ "ガルダ兵",   HUNTER,   18, 6,   1,  5,  5,  0,  2,  7, BOW },
-	{ "ガルダ兵",   THIEF,    16, 3,   1,  5,  9,  0,  4,  6, IRON_SWORD },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
-	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, AX },
+	//名前		   職業　　　 HP  STR SKI WLV AGI LUC DEF MOV TEAM    WEP
+	{ "マルス",	   LORD,     18, 5,   3,  5,  7,  7,  7,  7, PLAYER, RAPIRE },
+	{ "ジェイガン", PARADIN,  20, 7,   10, 10, 8,  1,  9, 10, PLAYER, IRON_SWORD },
+	{ "カイン",	   S_KINGHT, 18, 7,   5,  6,  7,  2,  7,  9, PLAYER, IRON_SWORD },
+	{ "アベル",	   S_KINGHT, 18, 6,   7,  6,  7,  2,  7,  7, PLAYER, IRON_SWORD },
+	{ "ドーガ",	   A_KINGHT, 18, 7,   3,  4,  3,  1,  11, 5, PLAYER, IRON_SWORD },
+	{ "ゴードン",   ARCHER,   16, 5,   3,  5,  4,  4,  6,  5, PLAYER, BOW },
+	{ "シーダ",	   P_KINGHT, 16, 3,   6,  7,  12,  9,  7, 8, PLAYER, IRON_SWORD },
+	{ "ガザック",   PIRATE,   24, 7,   3,  7,  8,  0,  6,  6, ENEMY,  STEAL_AX },
+	{ "ガルダ兵",   HUNTER,   18, 6,   1,  5,  5,  0,  2,  7, ENEMY,  BOW },
+	{ "ガルダ兵",   THIEF,    16, 3,   1,  5,  9,  0,  4,  6, ENEMY,  IRON_SWORD },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
+	{ "ガルダ兵",   PIRATE,   18, 5,   1,  5,  6,  0,  4,  6, ENEMY,  AX },
 };
 
 //カーソル
@@ -188,6 +220,16 @@ struct Cursor
 {
 	INT2 pos;
 }cursor;
+
+//ゲームの状態
+enum PHASE
+{
+	SELECT_UNIT,
+	SET_MOVE_POSITION,
+	SELECT_ATTACK_UNIT,
+	PHASE_MAX
+};
+PHASE phase;
 
 //座標を指定し、その座標にいるユニットを取得する
 int GetUnit(int x, int y)
@@ -203,6 +245,48 @@ int GetUnit(int x, int y)
 	//見つからなかったら-1
 	return -1;
 }
+
+//移動可能なセルを塗りつぶす
+void FillCanMoceCells(int unit_, int x, int y, int movePow)
+{
+	//画面外なら塗りつぶさない
+	if (x < 0 || x >= MAP_WIDTH || 
+		y < 0 || y >= MAP_HEIGHT)
+	{
+		return;
+	}
+	//敵がいる場所も塗りつぶさない
+	{
+		int unit = GetUnit(x, y);
+		if (unit >= 0 && units[unit].team != units[unit_].team)
+		{
+			return;
+		}
+	}
+	//引数で指定したユニットの移動力を取得
+	int moveCost = jobDesc[units[unit_].job].moveCosts[cells[y][x]];
+	//移動力が足りなければ終了
+	if (moveCost < 0)
+	{
+		return;
+	}
+	if (movePow < moveCost)
+	{
+		return;
+	}
+	fill[y][x] = true;
+	//塗りつぶしたらコストを払う
+	movePow -= moveCost;
+
+	for (int i = 0; i < DIRECTION_MAX; ++i)
+	{
+		int xx = x + directions[i][0];
+		int yy = y + directions[i][1];
+		FillCanMoceCells(unit_, xx, yy, movePow);
+	}
+}
+
+
 
 //ユニットの情報を画面に表示
 void DrawUnit(int unit)
@@ -235,6 +319,7 @@ void DrawMapData(INT2& Cursurpos)
 
 void Initialize()
 {
+	phase = SELECT_UNIT;
 	int pirateCnt = 0;//海賊の人数を格納
 	for (int y = 0; y < MAP_HEIGHT; ++y)
 	{
@@ -287,6 +372,10 @@ void UpDateDraw()
 				{
 					printf("◎");
 				}
+				else if (fill[y][x])
+				{
+					printf("■");
+				}
 				//ユニットの描画
 				else if (unit >= 0)
 				{
@@ -300,6 +389,14 @@ void UpDateDraw()
 			}
 			printf("\n");
 		}
+
+		switch (phase)
+		{
+		case SELECT_UNIT:		 printf("ユニットを選択してください\n"); break;
+		case SET_MOVE_POSITION:	 printf("移動先を設定してください\n"); break;
+		case SELECT_ATTACK_UNIT: printf("攻撃対象を選んでください\n"); break;
+		}
+		printf("\n");
 		//カーソルと同じ位置にあるオブジェクトの情報を画面に表示
 		int unit = GetUnit(cursor.pos.x, cursor.pos.y);
 		if (unit >= 0)
@@ -318,6 +415,36 @@ void UpDateDraw()
 		case 's': ++cursor.pos.y; break;
 		case 'a': --cursor.pos.x; break;
 		case 'd': ++cursor.pos.x; break;
+		//エンターキーを押したとき
+		case '\r': 
+			switch (phase)
+			{
+			case SELECT_UNIT: 
+			{
+				int unit = GetUnit(cursor.pos.x, cursor.pos.y);
+				if (unit < 0)
+				{
+					break;
+				}
+				else
+				{
+					//あるユニットの座標に対して上下左右に対して塗りつぶしができるか調べる
+					memset(fill, 0, sizeof fill);
+					for (int i = 0; i < DIRECTION_MAX; ++i)
+					{
+						int x = units[unit].pos.x + directions[i][0];
+						int y = units[unit].pos.y + directions[i][1];
+						FillCanMoceCells(unit, x, y, units[unit].move);
+					}
+				}
+				break;
+			}
+			case SET_MOVE_POSITION:  break;
+			case SELECT_ATTACK_UNIT: break;
+
+
+			}
+			break;
 		}
 		//カーソルが画面外に来たら端にワープする
 		cursor.pos.x = (MAP_WIDTH + cursor.pos.x) % MAP_WIDTH;
